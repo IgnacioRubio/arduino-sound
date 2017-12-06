ac8a4b7ebe6803ec3e79bae074627daaf7efde42
IgnacioRubio <ignaciorubio3@gmail.com>
2017-12-06 13:06:04 +0100

    Last uptadte

--- sketches/speaker-music/speaker-music.ino
+++ sketches/speaker-music/speaker-music.ino
@@ -4,42 +4,45 @@
  * Date: 27 November 2017
  * Description: Plays music using a speaker.
  */
-
-#define NOTE_C4 262
-#define NOTE_D4 294
-#define NOTE_E4 330
-#define NOTE_F4 349
-#define NOTE_G4 392
-#define NOTE_A4 440
-#define NOTE_B4 494
-#define NOTE_C5 523
+#include "pitches.h"
 
 const int kPinSpeaker = 9;
+
+#define NUM_NOTES 15
+
+const int notes[NUM_NOTES] = // a 0 represents rest
+{
+  NOTE_C4, NOTE_C4, NOTE_G4, NOTE_G4,
+  NOTE_A4, NOTE_A4, NOTE_G4, NOTE_F4,
+  NOTE_F4, NOTE_E4, NOTE_E4, NOTE_D4,
+  NOTE_D4, NOTE_C4, 0
+};
+
+const int beats[NUM_NOTES] = {1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 2, 4};
+const int beat_length = 300;
 
 void setup() {
   pinMode(kPinSpeaker, OUTPUT);
 }
 
-const int delayTime = 500;
-
 void loop() {
-  tone(kPinSpeaker, NOTE_C4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_D4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_E4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_F4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_G4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_A4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_B4, delayTime);
-  delay(delayTime);
-  tone(kPinSpeaker, NOTE_C5, delayTime);
-  delay(delayTime);
+  for (int i = 0; i < NUM_NOTES; i++) {
+    if (notes[i] == 0) {
+      delay(beats[i] * beat_length); //rest
+    }
+    else {
+      ourTone(notes[i], beats[i] * beat_length);
+    }
+    // pause between notes
+    noTone(kPinSpeaker);
+    delay(beat_length / 2);
+  }
 
   noTone(kPinSpeaker);
   delay(2000);
+}
+
+void ourTone(int freq, int duration) {
+  tone(kPinSpeaker, freq, duration);
+  delay(duration);
 }
